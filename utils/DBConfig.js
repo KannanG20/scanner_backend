1699c3fef404bc1500f8510bbf99544ee6a3a8e0
Kannan <kannan.g@peerxp.com>
2023-10-21 02:53:02 +0530

    latest updates

--- utils/DBConfig.js
+++ utils/DBConfig.js
@@ -7,13 +7,10 @@ dotenv.config()
 const user = process.env.SQL_USERNAME
 const password = process.env.SQL_PASSWORD
 const database = process.env.SQL_DATABASE
-
-console.log('User:', process.env.SQL_USERNAME);
-console.log('Password:', process.env.SQL_PASSWORD);
-console.log('Database:', process.env.SQL_DATABASE);
+const host = process.env.HOST
 
 const con = mysql.createConnection({
-    host: "localhost",
+    host: host,
     user: user,
     password: password,
     database: database
